6ecb044c9498fb35b8cc395e265463be3b400319
rakhargo <dhifiargo2004@gmail.com>
2024-05-10 20:38:24 +0700

    benerin merge

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,172 +1,3 @@
-// // ignore_for_file: use_super_parameters
-
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:kuis_api_kel29/provider/auth_api.dart';
-// import 'package:kuis_api_kel29/provider/item_api.dart';
-// import 'package:kuis_api_kel29/page/cart_page.dart';
-
-// class HomePage extends StatefulWidget {
-//   final int userID;
-//   final String accessToken;
-
-//   const HomePage({
-//     Key? key,
-//     required this.userID,
-//     required this.accessToken,
-//   }) : super(key: key);
-
-//   @override
-//   HomePageState createState() => HomePageState();
-// }
-
-// class HomePageState extends State<HomePage> {
-//   final _searchQueryController = TextEditingController();
-//   String namaUser = '';
-//   List<dynamic> listItem = [ ];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _fetchUsername();
-//     _fetchItems();
-//   }
-
-//   Future<void> _fetchUsername() async {
-//     final userId = widget.userID;
-//     final accessToken = widget.accessToken;
-//     final usernameResponse = await Provider.of<AuthAPI>(context, listen: false)
-//         .fetchUser(userId.toString(), 'Bearer $accessToken');
-//     setState(() {
-//       namaUser = usernameResponse['username'];
-//     });
-
-//   }
-
-//   Future<void> _fetchItems() async {
-//     final userId = widget.userID;
-//     final accessToken = widget.accessToken;
-//     final itemResponse = await Provider.of<ItemList>(context, listen: false)
-//         .fetchData('Bearer $accessToken');
-//     setState(() {
-//       listItem = itemResponse;
-//     });
-//   }
-
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: const Color.fromARGB(255, 36, 36, 36),
-//           elevation: 0,
-//           title: Text(
-//             "Selamat datang $namaUser",
-//             style: const TextStyle(color: Colors.white),
-//           ),
-//         ),
-//         body: Column(
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
-//           children: [
-//             Padding(
-//               padding: const EdgeInsets.all(10.0),
-//               child: TextField(
-//                 controller: _searchQueryController,
-//                 cursorColor: Colors.white,
-//                 style: const TextStyle(
-//                   color: Colors.white,
-//                 ),
-//                 decoration: const InputDecoration(
-//                   prefixIcon: Icon(Icons.search, color: Colors.white),
-//                   hintText: "Cari di yudart food",
-//                   hintStyle: TextStyle(
-//                     color: Colors.white,
-//                   ),
-//                   border: OutlineInputBorder(
-//                     borderSide: BorderSide(color: Colors.white),
-//                   ),
-//                   focusedBorder: OutlineInputBorder(
-//                     borderSide: BorderSide(
-//                       color: Colors.white,
-//                     ),
-//                   ),
-//                   filled: true,
-//                   fillColor: Color.fromARGB(255, 36, 36, 36),
-//                   contentPadding: EdgeInsets.symmetric(vertical: 10.0),
-//                 ),
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   Consumer<ItemList>(
-//                     builder: (context, item, child) {
-//                       return Column(
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         children: [
-//                           ListView.builder(
-//                             shrinkWrap: true,
-//                             itemCount: item.itemList.length,
-//                             itemBuilder: (context, index) {
-//                               final singleItem = item.itemList[index];
-//                               return ListTile(
-//                                   title: Text(singleItem.title),
-//                                   subtitle: Text("Deskripsi: ${singleItem.description}"),
-//                                   trailing: IconButton
-//                                   (
-//                                     icon: Icon(Icons.add, color: Colors.green),
-//                                     onPressed: ()
-//                                     {
-
-//                                     },
-//                                   ),
-//                                   // onTap: () {
-//                                   //   Navigator.of(context).push(
-//                                   //       MaterialPageRoute(builder: (conteUserApixt) => DetailProdukPage(
-            
-//                                   // id: singleItem.id)));
-//                                   // }
-//                                   );
-//                             },
-//                           ),
-//                         ],
-//                       );
-//                     },
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//         floatingActionButton: FloatingActionButton(
-//           onPressed: () {
-//             // Tambahkan aksi ketika ikon keranjang di bawah diklik
-//             Navigator.of(context).push(
-//             MaterialPageRoute(
-//               builder: (context) => CartPage(
-
-//                 // userID: userID,
-//                 // accessToken: accessToken,
-//               ),
-//             ),
-//           );
-//           },
-//           child: Badge
-//           (
-//             // label: Text('${cart.length}'),
-//             child: Icon(Icons.shopping_cart)),
-//         ),
-//         floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:kuis_api_kel29/provider/auth_api.dart';
@@ -219,6 +50,20 @@ class HomePageState extends State<HomePage> {
       listItem = itemResponse;
     });
   }
+
+  void createCart(String itemId) {
+    Map<String, dynamic> newCart = {
+      'user_id': widget.userID, // UserID
+      'item_id': itemId, // ItemID
+      'quantity': 1, // Quantity
+    };
+
+    // Tambahkan keranjang baru ke wholeCarts
+    setState(() {
+      wholeCarts.add(newCart);
+    });
+  }
+
 
 
   @override
@@ -283,12 +128,13 @@ class HomePageState extends State<HomePage> {
                               return ListTile(
                                   title: Text(singleItem.title),
                                   subtitle: Text("Deskripsi: ${singleItem.description}"),
-                                  // onTap: () {
-                                  //   Navigator.of(context).push(
-                                  //       MaterialPageRoute(builder: (conteUserApixt) => DetailProdukPage(
-            
-                                  // id: singleItem.id)));
-                                  // }
+                                  trailing: IconButton(
+                                    icon: Icon(Icons.add, color: Colors.green),
+                                      onPressed: () {
+                                        // Tambahkan keranjang baru saat tombol "+" ditekan
+                                        createCart(singleItem.id);
+                                      },
+                                    ),
                                   );
                             },
                           ),
